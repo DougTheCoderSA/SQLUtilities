3918c8ed9890971aa1718599cba5497cab8ae1ee
Douglas Crole <fiddlesticks0002@gmail.com>
2020-08-26 16:53:53 +0200

    Renamed Main Form

--- SQLSelect/FormMainWindow.cs
+++ SQLSelect/FormMainWindow.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SQLSelect
 {
-    public partial class Form1 : Form
+    public partial class FormMainWindow : Form
     {
-        public Form1()
+        public FormMainWindow()
         {
             InitializeComponent();
         }
